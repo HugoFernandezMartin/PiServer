fe041c21fd1fcbedc6c3ab18889ad5bb49600396
HugoFernandezMartin <fernanmhugo@gmail.com>
2025-07-06 03:47:11 +0200

    Sistema de Login Creado. Sin cifrado

---
+++ server/src/gestion_cliente.rs
@@ -0,0 +1,55 @@
+use std::io::Error;
+use std::process::exit;
+use std::sync::Arc;
+
+use crate::gestor_usuarios::{Autenticable, Credenciales, GestorUsuarios};
+use tokio::io::AsyncReadExt;
+use tokio::net::TcpStream;
+
+pub async fn hilo_cliente(gestor_usuarios: Arc<GestorUsuarios>, mut socket: TcpStream) {
+    //Obtener credenciales
+    let credenciales: Credenciales = match recibir_credenciales(&mut socket).await {
+        Ok(c) => c,
+        Err(e) => {
+            eprintln!("Error al recibir los credenciales: {e}");
+            exit(1);
+        }
+    };
+    println!(
+        "Credenciales recibidos: \n    Nombre: {}\n    Password: {}",
+        credenciales.get_nombre(),
+        credenciales.get_password()
+    );
+
+    //Intentar iniciar sesion
+    match gestor_usuarios
+        .iniciar_sesion(credenciales.get_nombre(), credenciales.get_password())
+        .await
+    {
+        Ok(msg) => {
+            println!("{msg}");
+        }
+        Err(e) => {
+            eprintln!("Error de autenticación: {e}");
+        }
+    }
+}
+
+async fn recibir_credenciales(socket: &mut TcpStream) -> Result<Credenciales, Error> {
+    let mut len_buf = [0u8; 1];
+    socket.read_exact(&mut len_buf).await?;
+    let len = len_buf[0] as usize;
+    let mut buf = vec![0u8; len];
+    socket.read_exact(&mut buf).await?;
+
+    let nombre = String::from_utf8_lossy(&buf).to_string();
+
+    socket.read_exact(&mut len_buf).await?;
+    let len = len_buf[0] as usize;
+    let mut buf = vec![0u8; len];
+    socket.read_exact(&mut buf).await?;
+
+    let password = String::from_utf8_lossy(&buf).to_string();
+
+    Ok(Credenciales::new(nombre, password))
+}
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,9 +1,24 @@
+mod gestion_cliente;
+mod gestor_usuarios;
+use std::io::Error;
+use std::sync::Arc;
+
+use crate::gestion_cliente::hilo_cliente;
+use crate::gestor_usuarios::Autenticable;
+use gestor_usuarios::GestorUsuarios;
 use tokio::io::AsyncReadExt;
-use tokio::net::TcpListener;
+use tokio::net::{TcpListener, TcpStream};
 
 #[tokio::main]
 async fn main() {
     let addr = "127.0.0.1:8080";
+
+    //Inicializar gestor de usuarios
+    let mut gu = GestorUsuarios::new();
+    gu.registrar_usuario("Hugo".to_string(), "1234".to_string())
+        .await;
+    let gestor_usuarios = Arc::new(gu);
+
     //Inicializar conexion TCP
     let listener = match TcpListener::bind(addr).await {
         Ok(l) => l,
@@ -13,21 +28,29 @@ async fn main() {
         }
     };
 
-    match listener.accept().await {
-        Ok((mut socket, _socket_addr)) => {
-            let mut buf = [0; 1024];
-            let n = match socket.read(&mut buf).await {
-                Ok(n) => n,
-                Err(e) => {
-                    eprintln!("Unable to read: {e}");
-                    return;
+    println!("Server listening {}", addr);
+
+    loop {
+        match listener.accept().await {
+            Ok((mut socket, socket_addr)) => {
+                match handshake(&mut socket).await {
+                    Ok(msg) => println!("{}: {}", socket_addr, msg),
+                    Err(e) => eprintln!("Unable to established connection: {e}"),
                 }
-            };
-            let msg = String::from_utf8_lossy(&buf[..n]).to_string();
-            println!("{}", msg);
-        }
-        Err(e) => {
-            eprintln!("Unable to connect to client: {e}");
+                let _ = tokio::spawn(hilo_cliente(gestor_usuarios.clone(), socket));
+            }
+            Err(e) => {
+                eprintln!("Unable to connect to client: {e}");
+            }
         }
     }
+}
+
+async fn handshake(socket: &mut TcpStream) -> Result<String, Error> {
+    let mut len_buf = [0u8; 1];
+    socket.read_exact(&mut len_buf).await?;
+    let len = len_buf[0] as usize;
+    let mut buf = vec![0u8; len];
+    socket.read_exact(&mut buf).await?;
+    Ok(String::from_utf8_lossy(&buf).to_string())
 }
